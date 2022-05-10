ad64cd20d9ddab9e6b51cb52bb63a56c394c0139
kacper <kacper.tarlowski20@gmail.com>
2022-05-10 03:36:37 +0200

    Add project files.

---
+++ BookLibrary.Core/Domain/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Core.Domain
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
---
+++ BookLibrary.Core/Domain/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Core.Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Description { get; set; }
+        public string Language { get; set; }
+        public int NumberOfPages { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        public int PublisherId { get; set; }
+        public Publisher Publisher { get; set; }
+
+        public int? BookSeriesId { get; set; }
+        public BookSeries BookSeries { get; set; }
+
+        public ICollection<Category> Categories { get; set; }
+    }
+}
